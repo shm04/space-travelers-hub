b462416804dc3880ba07023eedd14414b1ca892f
devalibello <alibelloimoukhuede@gmail.com>
2023-07-05 21:51:52 +0300

    Add reducer functions

--- src/redux/dragons/dragonsSlice.js
+++ src/redux/dragons/dragonsSlice.js
@@ -1,13 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+
+const url = "https://api.spacexdata.com/v3/dragons";
 
 const initialState = {
-    num = 0
+    loading: false,
+    dragons: [],
+    error: ''
 }
+
+//createAsync automatically generates actions of pending, fulfiled or rejected 
+const fetchDragons = createAsyncThunk("space/fetchDragons", () => {
+    axios.get(url).then(response => console.log(response.data))
+});
+
 const dragonSlice = createSlice({
     name: "dragon",
     initialState,
     reducers: {},
-    extraReducers: {
-        
+    extraReducers: (builder) => {
+        builder.addCase(fetchDragons.pending, state => {
+            state.loading = true
+        })
+
+        builder.addCase(fetchDragons.fulfilled, (state, action) => {
+            state.loading = false,
+            state.dragons = action.payload,
+            state.error = ""
+        })
+
+        builder.addCase(fetchDragons.rejected, (state, action) => {
+            state.loading = false,
+            state.dragons = [],
+            state.error = action.error.message
+        })
     }
 })
+
+export default dragonSlice.reducer;
+export {fetchDragons};
