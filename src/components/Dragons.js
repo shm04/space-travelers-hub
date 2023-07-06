5b350bf638cf40d8db0359599e215a01d1618e67
devalibello <alibelloimoukhuede@gmail.com>
2023-07-06 20:04:22 +0300

    Add dragon reservation

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,38 +1,104 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Badge } from 'react-bootstrap';
+// import { fetchDragons, reserveDragon } from '../redux/dragons/dragonsSlice';
+// import '../styles/dragon.css';
+
+// const Dragons = () => {
+//   const [reserved, setReserved] = useState(false);
+
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(fetchDragons());
+//   }, [dispatch]);
+
+//   const setReservedValue = (id) => {
+//     setReserved(!reserved);
+//     dispatch(reserveDragon(id))
+//   };
+
+//   const dragon = useSelector((state) => state.dragon.dragons);
+
+//   return (
+//     <>
+//       {dragon.slice(0, 2).map((element) => (
+//         <div className="dragon-section" key={element.id}>
+//           <img src={element.flickr_images} alt="dragon" className="dragon-picture" />
+//           <div className="dragon-details">
+//             <h3>{element.name}</h3>
+//             <p className="dragon-writeup">
+//               {reserved && <Badge bg="success">reserved</Badge>}
+//               {' '}
+//               {element.description}
+//             </p>
+//             <button
+//               type="submit"
+//               className="reserve-btn"
+//               onClick={() => setReservedValue(element.id)}
+//             >
+//               Reserve Dragon
+//             </button>
+//           </div>
+//         </div>
+//       ))}
+//     </>
+
+//   );
+// };
+// export default Dragons;
+
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Badge } from 'react-bootstrap';
 import { fetchDragons, reserveDragon } from '../redux/dragons/dragonsSlice';
 import '../styles/dragon.css';
 
 const Dragons = () => {
-  const dispatch = useDispatch();
+  const [reserved, setReserved] = useState([]);
 
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchDragons());
   }, [dispatch]);
 
+  const setReservedValue = (id) => {
+    const updatedReserved = [...reserved];
+    const index = updatedReserved.indexOf(id);
+    if (index !== -1) {
+      updatedReserved.splice(index, 1);
+    } else {
+      updatedReserved.push(id);
+    }
+    setReserved(updatedReserved);
+    dispatch(reserveDragon(id));
+  };
+
   const dragon = useSelector((state) => state.dragon.dragons);
+
   return (
     <>
       {dragon.slice(0, 2).map((element) => (
         <div className="dragon-section" key={element.id}>
           <img src={element.flickr_images} alt="dragon" className="dragon-picture" />
           <div className="dragon-details">
-            <h2>{element.name}</h2>
+            <h3>{element.name}</h3>
             <p className="dragon-writeup">
+              {reserved.includes(element.id) && <Badge bg="success">reserved</Badge>}
+              {' '}
               {element.description}
             </p>
             <button
               type="submit"
-              className="reserve-btn"
-              onClick={() => dispatch(reserveDragon(element.id))}
+              className={`reserve-btn ${reserved.includes(element.id) ? 'unreserve' : ''}`}
+              onClick={() => setReservedValue(element.id)}
             >
-              Reserve Dragon
+              {reserved.includes(element.id) ? 'Cancel Reservation' : 'Reserve Dragon'}
             </button>
           </div>
         </div>
       ))}
     </>
-
   );
 };
+
 export default Dragons;
