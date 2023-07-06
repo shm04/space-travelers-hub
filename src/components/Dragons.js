35a5f27f180fe4c3a3f287b5a248c8089c146531
devalibello <alibelloimoukhuede@gmail.com>
2023-07-06 14:14:48 +0300

    Add styling to fetch data

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDragons } from '../redux/dragons/dragonsSlice';
+import '../styles/dragon.css';
 
 const Dragons = () => {
   const dispatch = useDispatch();
@@ -14,13 +15,13 @@ const Dragons = () => {
     <>
       {dragon.slice(0, 3).map((element) => (
         <div className="dragon-section" key={element.id}>
-          <img src={element.flickr_images} alt="" />
+          <img src={element.flickr_images} alt="dragon" className="dragon-picture" />
           <div className="dragon-details">
             <h2>{element.name}</h2>
             <p className="dragon-writeup">
               {element.description}
             </p>
-            <button type="submit">Reserve Dragon</button>
+            <button type="submit" className="reserve-btn">Reserve Dragon</button>
           </div>
         </div>
       ))}
